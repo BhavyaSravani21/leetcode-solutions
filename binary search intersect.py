7ddd51d66059819c3d58cde5817f970b77bf32e7
BhavyaSravani <bhavyasravani21@gmail.com>
2025-12-13 23:45:11 +0530

    Implement intersection of two arrays.py

---
+++ binary search intersect.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intersect(self, nums1, nums2):
+        count = {}
+        result = []
+
+        # Count elements of nums1
+        for num in nums1:
+            count[num] = count.get(num, 0) + 1
+
+        # Check elements of nums2
+        for num in nums2:
+            if num in count and count[num] > 0:
+                result.append(num)
+                count[num] -= 1
+
+        return result
