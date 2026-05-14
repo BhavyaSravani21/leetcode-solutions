ee0933843430fd3f61341023dba6dbff562feeae
BhavyaSravani <bhavyasravani21@gmail.com>
2026-05-14 23:37:39 +0530

    Implement canVisitAllRooms method in Solution class.py

---
+++ keys and rooms.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        visited = set([0])
+        q = [0]
+
+        while q:
+            room = q.pop(0)
+
+            for key in rooms[room]:
+                if key not in visited:
+                    visited.add(key)
+                    q.append(key)
+
+        return len(visited) == len(rooms)
+        
